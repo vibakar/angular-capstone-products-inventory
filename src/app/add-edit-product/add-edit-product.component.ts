cd98dc350e31fbfbcb2a8e418a72ff13a934a0b5
sundaresan <sundaresan.v41@wipro.com>
2019-08-26 17:43:56 +0530

    form added for add product

--- src/app/add-edit-product/add-edit-product.component.ts
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-product.component.css']
 })
 export class AddEditProductComponent implements OnInit {
-
+  products: any[]=[]
   constructor() { }
 
   ngOnInit() {
   }
+  addProduct = (product,manufacturer,price,quantity,selected) =>{
+    console.log(product,manufacturer,price,quantity,selected,"getting value")
+    this.products.push({product,selected,manufacturer,price,quantity})
+    
+  }
 
+
+}
+
+
+export interface products {
+  product:string,
+  manufacturer:string,
+  price:number,
+  quantity:number,
+  selected:string
 }
