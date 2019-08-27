7b20774ca77003d750015a2b619ea3259a343cee
vibakar <vibakar23@gmail.com>
2019-08-27 09:15:33 +0530

    added description and imge url

--- src/app/add-edit-product/add-edit-product.component.ts
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -6,25 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit-product.component.css']
 })
 export class AddEditProductComponent implements OnInit {
-  products: any[]=[]
   constructor() { }
 
   ngOnInit() {
   }
-  addProduct = (product,manufacturer,price,quantity,selected) =>{
-    console.log(product,manufacturer,price,quantity,selected,"getting value")
-    this.products.push({product,selected,manufacturer,price,quantity})
-    
+  addProduct = (data) =>{
+    console.log(data.value, "getting value")
   }
-
-
 }
 
-
 export interface products {
-  product:string,
-  manufacturer:string,
-  price:number,
-  quantity:number,
-  selected:string
+  product:string;
+  category:string;
+  manufacturer:string;
+  descripiton: string;
+  imageUrl: string;
+  price:number;
+  quantity:number;
 }
