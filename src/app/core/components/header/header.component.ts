d4e33eb7c2a9f0babf9cec5ddb949f85ca9cf9be
vibakar <vibakar23@gmail.com>
2019-09-03 09:03:50 +0530

    Designed Login and Signup form

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { CoreService } from '../../services/core.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'header',
@@ -10,7 +12,7 @@ import { CoreService } from '../../services/core.service';
 })
 export class HeaderComponent implements OnInit {
   isLoggedIn:boolean = false;
-  constructor(private router: Router, private coreService: CoreService) { }
+  constructor(private router: Router, private coreService: CoreService, private dialog: MatDialog) { }
 
   ngOnInit() {
   	this.isLoggedIn = this.coreService.isLoggedIn();
@@ -18,5 +20,13 @@ export class HeaderComponent implements OnInit {
 
   goToProfile() {
   	this.router.navigate(['/profile']);
+  }
+
+  openLoginDialog(): void {
+    const dialogRef = this.dialog.open(LoginComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,25 +4,37 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from './components/header/header.component';
 import { CoreService } from './services/core.service';
 import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
   	HeaderComponent,
-  	ProfileComponent
+  	ProfileComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     HttpClientModule,
     MatToolbarModule,
-    MatMenuModule
+    MatMenuModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
     CoreService
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   exports: [
     HeaderComponent,
